9fa0f68d4824e382c90d59e548467a3233312c8d
derrickturk <dwt@terminusdatascience.com>
2018-06-09 23:49:06 -0500

    Ok, let's see how this goes.

--- src/Language/MinPS/Check.hs
+++ src/Language/MinPS/Check.hs
@@ -1,16 +1,17 @@
 {-# LANGUAGE GADTs, DataKinds, KindSignatures, StandaloneDeriving #-}
-{-# LANGUAGE LambdaCase, GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE FlexibleContexts, LambdaCase, GeneralizedNewtypeDeriving #-}
 
 module Language.MinPS.Check (
     TypeError
+  , MonadCheck(..)
+  , check
+  , infer
   , TypedRecEnv
   , emptyTypedRecEnv
   , Check
   , runCheck
   , evalCheck
   , execCheck
-  , check
-  , infer
 ) where
 
 import Language.MinPS.Syntax
@@ -20,6 +21,7 @@ import Language.MinPS.Eval
 import Control.Monad.State
 import Control.Monad.Except
 import qualified Data.Sequence as S
+import qualified Data.Set as Set
 import qualified Data.Text as T
 
 -- we're going to use the Closure structure to store the types
@@ -31,48 +33,23 @@ import qualified Data.Text as T
 
 data TypeError =
     Mismatch (Term 'Checked) (Term 'Checked)
+  | Undeclared T.Text
+  | Undefined T.Text
+  | DuplicateDeclare T.Text
+  | DuplicateDefine T.Text
   deriving Show
 
-data TypedRecEnv = TypedRecEnv
-  { getTypeEnv :: S.Seq (T.Text, Closure, Term 'Checked)
-  , getValueEnv :: S.Seq (T.Text, Closure, Term 'Checked)
-  , nextBinding :: Int
-  }
+-- wait, do I want this?
+class (MonadEval m, MonadError TypeError m) => MonadCheck m where
+  getBoundType :: RecBinding -> m (Maybe (T.Text, Closure, Term 'Checked))
+  addBoundType :: (T.Text, Closure, Term 'Checked) -> m RecBinding
 
-emptyTypedRecEnv :: TypedRecEnv
-emptyTypedRecEnv = TypedRecEnv S.empty S.empty 0
-
-newtype Check a = Check { getCheck :: ExceptT TypeError (State TypedRecEnv) a }
-  deriving ( Functor
-           , Applicative
-           , Monad
-           , MonadError TypeError
-           , MonadState TypedRecEnv
-           )
-runCheck :: Check a -> TypedRecEnv -> (Either TypeError a, TypedRecEnv)
-runCheck = runState . runExceptT . getCheck
-
-evalCheck :: Check a -> TypedRecEnv -> Either TypeError a
-evalCheck = evalState . runExceptT . getCheck
-
-execCheck :: Check a -> TypedRecEnv -> TypedRecEnv
-execCheck = execState . runExceptT . getCheck
-
-{-- not convinced this can work; not convinced it needs to
-liftEval :: Eval a -> Check a
-liftEval m = do
-  TypedRecEnv tyE valE next <- get
-  let (x, RecEnv valE' next') = runEval m (RecEnv valE next)
-  put $ TypedRecEnv tyE valE' next'
-  pure x
---}
-
-check :: Term 'Unchecked -> Term 'Checked -> Check (Term 'Checked)
+check :: Term 'Unchecked -> Value -> Check (Term 'Checked)
 check = check' []
 
 check' :: Closure
        -> Term 'Unchecked
-       -> Term 'Checked
+       -> Value
        -> Check (Term 'Checked)
 check' c (Let ctxt t) ty = do
   (ctxt', c') <- checkContext ctxt c
@@ -88,18 +65,98 @@ infer' = undefined
 checkContext :: Context 'Unchecked
              -> Closure
              -> Check (Context 'Checked, Closure)
-checkContext = go [] where
-  go :: [(T.Text, Int)]
+checkContext ctxt clos = go [] ctxt clos >>= checkUnique where
+  go :: [(T.Text, RecBinding)]
      -> Context 'Unchecked
      -> Closure
      -> Check (Context 'Checked, Closure)
 
-  go _ [] c = pure ([], c)
+  go _ [] c = pure ([], c) -- TODO: all-defined check?
 
   go n ((Declare x ty):rest) c = do
-    next <- gets nextBinding
-    env <- gets getTypeEnv
-    -- let c' = MkLabel
-    undefined -- TODO
+    ty' <- check' c ty VType
+    binding <- addBoundType (x, c, ty')
+    (rest', c') <- go ((x, binding):n) rest (binding :- c)
+    pure ((Declare x ty'):rest', c')
 
-  go n ((Define x t):rest) c = go n rest c -- TODO fix
+  go n ((Define x t):rest) c = do
+    case lookup x n of
+      Nothing -> throwError $ Undeclared x
+      Just binding -> getBoundType binding >>= \case
+        Nothing -> throwError $ Undeclared x -- shouldn't happen
+        Just (_, cTy, ty) -> do
+          ty' <- eval' cTy ty
+          t' <- check' c t ty'
+          updateBoundTerm binding (x, c, t')
+          (rest', c') <- go n rest c
+          pure ((Define x t'):rest', c')
+
+checkUnique :: (Context 'Checked, Closure) -> Check (Context 'Checked, Closure)
+checkUnique (ctxt, c) = go Set.empty Set.empty ctxt >> pure (ctxt, c) where
+  go decls defns [] = case Set.lookupMin (Set.difference decls defns) of
+    Nothing -> pure ()
+    Just x -> throwError (Undefined x)
+  go decls defns ((Declare x _):rest) = if Set.member x decls
+    then throwError (DuplicateDeclare x)
+    else go (Set.insert x decls) defns rest
+  go decls defns ((Define x _):rest) = if Set.member x defns
+    then throwError (DuplicateDefine x)
+    else go decls (Set.insert x defns) rest
+
+data TypedRecEnv = TypedRecEnv
+  { getTypeEnv :: S.Seq (T.Text, Maybe (Closure, Term 'Checked))
+  , getValueEnv :: S.Seq (T.Text, Maybe (Closure, Term 'Checked))
+  , _nextBinding :: Int
+  }
+
+emptyTypedRecEnv :: TypedRecEnv
+emptyTypedRecEnv = TypedRecEnv S.empty S.empty 0
+
+newtype Check a = Check { getCheck :: ExceptT TypeError (State TypedRecEnv) a }
+  deriving ( Functor
+           , Applicative
+           , Monad
+           , MonadError TypeError
+           , MonadState TypedRecEnv
+           )
+
+instance MonadEval Check where
+  getBoundTerm (MkRecBinding i) = do
+    rec <- gets (S.lookup i . getValueEnv)
+    pure $ do
+      (x, val) <- rec
+      (c, t) <- val
+      pure $ (x, c, t)
+
+  addBoundTerm (x, c, t) = do
+    TypedRecEnv tyEnv tmEnv next <- get
+    put $ TypedRecEnv
+      (tyEnv S.:|> (x, Nothing)) (tmEnv S.:|> (x, Just (c, t))) (next + 1)
+    pure $ MkRecBinding next
+
+  updateBoundTerm (MkRecBinding i) (x, c, t) = do
+    TypedRecEnv tyEnv tmEnv next <- get
+    put $ TypedRecEnv tyEnv (S.update i (x, Just (c, t)) tmEnv) next
+
+instance MonadCheck Check where
+  getBoundType (MkRecBinding i) = do
+    rec <- gets (S.lookup i . getTypeEnv)
+    pure $ do
+      (x, val) <- rec
+      (c, t) <- val
+      pure $ (x, c, t)
+
+  addBoundType (x, c, ty) = do
+    TypedRecEnv tyEnv tmEnv next <- get
+    put $ TypedRecEnv
+      (tyEnv S.:|> (x, Just (c, ty))) (tmEnv S.:|> (x, Nothing)) (next + 1)
+    pure $ MkRecBinding next
+
+runCheck :: Check a -> TypedRecEnv -> (Either TypeError a, TypedRecEnv)
+runCheck = runState . runExceptT . getCheck
+
+evalCheck :: Check a -> TypedRecEnv -> Either TypeError a
+evalCheck = evalState . runExceptT . getCheck
+
+execCheck :: Check a -> TypedRecEnv -> TypedRecEnv
+execCheck = execState . runExceptT . getCheck
