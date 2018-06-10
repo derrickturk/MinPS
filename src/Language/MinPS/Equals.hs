a6c46fefb84e6233d7353cfaa1dc6b5c342bf38d
derrickturk <dwt@terminusdatascience.com>
2018-06-10 16:00:26 -0500

    Getting there... but I have discovered another secret of the shameful dogs.

--- src/Language/MinPS/Equals.hs
+++ src/Language/MinPS/Equals.hs
@@ -6,6 +6,7 @@ module Language.MinPS.Equals (
 ) where
 
 import Language.MinPS.Environment
+import Language.MinPS.Syntax
 import Language.MinPS.Value
 import Language.MinPS.Eval
 
@@ -13,9 +14,35 @@ import Control.Monad.State
 
 class Equals a where
   (.=.) :: MonadState Env m => a -> a -> m Bool
+infix 3 .=.
 
 instance Equals Value where
-  x .=. y = pure False
+  VType .=. VType = pure True
+  VEnum xs .=. VEnum ys = pure $ xs == ys
+  VEnumLabel x .=. VEnumLabel y = pure $ x == y
+  VNeutral x .=. VNeutral y = x .=. y
+
+  VPi x ((tyX, t) :@ c) .=. VPi y ((tyY, u) :@ d) = do
+    eqTy <- join $ (.=.) <$> eval' (tyX :@ c) <*> eval' (tyY :@ d)
+    eqTU <- equalsBound (Just (tyX :@ c)) x (t :@ c) y (u :@ d)
+    pure $ eqTy && eqTU
+  _ .=. _ = pure False
+
+equalsBound :: MonadState Env m
+            => Maybe (Closure (Term 'Checked))
+            -> Ident
+            -> Closure (Term 'Checked)
+            -> Ident
+            -> Closure (Term 'Checked)
+            -> m Bool
+equalsBound ty x (t :@ c) y (u :@ d) = do
+  iX <- declareE ty
+  vT <- eval' (t :@ (x, iX):c)
+  vU <- eval' (u :@ (y, iX):d)
+  vT .=. vU
 
 instance Equals Neutral where
-  x .=. y = pure False
+  NVar x .=. NVar y
+    | x == y = pure True
+    | otherwise = error "oh god"
+  _ .=. _ = pure False
