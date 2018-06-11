1ce85f48ad92fcdda87b9c00a12c3d21e6f70b43
derrickturk <dwt@terminusdatascience.com>
2018-06-11 17:47:29 -0500

    Unspeakable horrors.

--- src/Language/MinPS/Environment.hs
+++ src/Language/MinPS/Environment.hs
@@ -5,7 +5,9 @@ module Language.MinPS.Environment (
     Scope
   , Closure(..)
   , EnvEntry(..)
+  , Constraints(..)
   , Env
+  , setEnv
   , lookupSE
   , lookupE
   , declareE
@@ -19,6 +21,7 @@ module Language.MinPS.Environment (
 
 import Language.MinPS.Syntax
 import Language.MinPS.Closure
+import Language.MinPS.Value
 
 import Control.Monad.State
 import qualified Data.Sequence as S
@@ -41,8 +44,18 @@ data EnvEntry = EnvEntry { envName :: Ident
                          , envValue :: Either Int (Closure (Term 'Checked))
                          } deriving Show
 
-newtype Env = Env { getEnv :: S.Seq EnvEntry }
+data Constraints =
+    Inconsistent
+  | Constraints [(Neutral, Label)]
   deriving Show
+
+data Env = Env { getEnv :: S.Seq EnvEntry
+               , getConstraints :: Constraints
+               }
+  deriving Show
+
+setEnv :: S.Seq EnvEntry -> Env -> Env
+setEnv e (Env _ consts) = Env e consts
 
 lookupSE :: Ident -> Scope -> Env -> Maybe (EnvEntry, Int)
 lookupSE x s e = lookup x s >>= \i -> lookupE i e >>= \ee -> pure (ee, i)
@@ -57,20 +70,20 @@ declareE :: MonadState Env m
 declareE x ty = do
   env <- gets getEnv
   let next = S.length env
-  put $ Env $ env S.:|> EnvEntry x ty (Left next)
+  modify $ setEnv (env S.:|> EnvEntry x ty (Left next))
   pure next
 
 defineE :: MonadState Env m => Int -> Closure (Term 'Checked) -> m ()
 defineE i t = do
   env <- gets getEnv
-  put $ Env $ S.adjust' (install t) i env
+  modify $ setEnv (S.adjust' (install t) i env)
   where
     install val (EnvEntry x ty _) = EnvEntry x ty (Right val)
 
 repointE :: MonadState Env m => Int -> Int -> m ()
 repointE i j = do
   env <- gets getEnv
-  put $ Env $ S.adjust' (repoint j) i env
+  modify $ (setEnv (S.adjust' (repoint j) i env))
   where
     repoint idx (EnvEntry x ty _) = EnvEntry x ty (Left idx)
 
@@ -81,7 +94,7 @@ emptyC :: a -> Closure a
 emptyC = (:@ emptyS)
 
 emptyE :: Env
-emptyE = Env S.empty
+emptyE = Env S.empty (Constraints [])
 
 locally :: MonadState s m => m a -> m a
 locally m = get >>= \s -> m >>= \x -> put s >> pure x
